4f5e11dc671ab662af682d89258c0b2bd9f8c38c
mukesh29kannan <152781611+mukesh29kannan@users.noreply.github.com>
2025-05-21 14:38:50 +0530

    Update index.js
    Temp reduced

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.post('/api/sensor-data', (req, res) => {
 
 app.get('/api/sensor-data', (req, res) => {
   console.log
-  res.json({...sensorData,temp:sensorData.temp});
+  res.json({...sensorData,temp:sensorData.temp-25});
 });
 
 app.get('/api/control', (req, res) => {
